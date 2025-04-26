d5020d4b89ba5830ad4c2f5af34cd47590a3c5eb
Asmaa Alaghbari <73389316+Asmaa-Alaghbari@users.noreply.github.com>
2025-04-26 14:33:17 +0300

    Tailwind CSS

--- ecommerce/app/products/[id]/page.tsx
+++ ecommerce/app/products/[id]/page.tsx
@@ -29,16 +29,22 @@ export default function ProductDetailPage({
   }
 
   return (
-    <>
-      <h1>{product.name}</h1>
-      <p>{product.description}</p>
-      <p>${product.price}</p>
-      <Image
-        src={"/" + product.imageUrl}
-        alt={product.name}
-        width={200}
-        height={200}
-      />
-    </>
+    <div className="cantainer mx-auto p-8 flex flex-col md:flex-row">
+      <div className="md:w-1/2 mb-4 md:mb-0 md:mr-8">
+        <Image
+          src={"/" + product.imageUrl}
+          alt={product.name}
+          width={500}
+          height={500}
+          className="w-full h-auto rounded-lg shadow-md"
+        />
+      </div>
+      <div className="md:w-1/2">
+        <h1 className="text-4xl font-bold mb-4">{product.name}</h1>
+        <p className="text-2xl text-gray-600 mb-6">${product.price}</p>
+        <h3 className="text-2xl font-semibold mb-2">Description</h3>
+        <p className="text-gray-700">{product.description}</p>
+      </div>
+    </div>
   );
 }
